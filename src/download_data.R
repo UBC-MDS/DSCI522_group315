bcd292925bbb5833cdf2c78ef129a02ee9c37960
csbing86 <csbing86@hotmail.com>
2020-01-19 05:47:19 -0800

    src/download_data.R

---
+++ src/download_data.R
@@ -0,0 +1,32 @@
+# author: DSCI_522_group_315
+# date: 2020-01-18
+
+"This script downloads data csv data from the web and save the file to a local filepath in csv format.
+
+Usage: download_data.R --url=<url> --out_file=<out_file>
+
+Options:
+--url=<url>              URL of the data (in csv format)
+--out_file=<out_file>    Path (including filename) to locally save the file
+
+Example: Rscript download_data.R --url https://github.com/SamEdwardes/ufc-data/raw/master/data.csv --out_file /Users/username/Desktop/Block4/filename.csv
+
+" -> doc
+
+library(tidyverse)
+library(RCurl)
+library(docopt)
+
+opt <- docopt(doc)
+
+main <- function(url, out_file){
+ 
+   if (url.exists(url)==FALSE){
+       stop("url doesn't exist")
+     }
+  
+    data <- read_csv(url)
+    write_csv(data, out_file)
+}
+
+main(opt$url, opt$out_file)
