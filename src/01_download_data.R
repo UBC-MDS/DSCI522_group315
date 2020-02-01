61487dd55e8517421a122f017920b5217850a8f6
sweber15 <sarah.weber@live.ca>
2020-01-31 18:31:56 -0800

    updated download script with tests and usage description

--- src/01_download_data.R
+++ src/01_download_data.R
@@ -3,22 +3,38 @@
 
 "This script downloads data csv data from the web and save the file to a local filepath in csv format.
 
-Usage: download_data.R --url=<url> --out_file=<out_file>
+Usage: src/01_download_data.R --url=<url> --out_file=<out_file>
 
 Options:
+
 --url=<url>              URL of the data (in csv format)
 --out_file=<out_file>    Path (including filename) to locally save the file
 
 Example: Rscript src/01_download_data.R --url=https://github.com/SamEdwardes/ufc-data/raw/master/raw_total_fight_data.csv --out_file=data/01_raw/raw_total_fight_data.csv
-
 " -> doc
 
 library(docopt)
+library(RCurl)
+library(testthat)
+library(stringr)
 
 opt <- docopt(doc)
 
 main <- function(url, out_file){
-    download.file(url = url, destfile = out_file)
+  
+  # Test to see if URL Exists
+  if (!url.exists(url)==FALSE){
+    stop("url doesn't exist")
+  }
+  
+  # Tests that the output will be a csv file
+  test_that("Output path must be a csv file.", {
+    expect_true(str_sub(out_file, -4, -1) == ".csv")
+  })
+  
+  # Download file
+  download.file(url = url, destfile = out_file)
 }
 
+  
 main(opt$url, opt$out_file)
